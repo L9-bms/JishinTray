2c544a415ddce0ba9a6b043a516b0932a9123a6f
callum <ccallum.wong@gmail.com>
2025-03-11 22:09:26 +1100

    clean up

--- src/test/java/com/callumwong/jishintray/P2PQuakeClientTest.java
+++ src/test/java/com/callumwong/jishintray/P2PQuakeClientTest.java
@@ -3,17 +3,12 @@ package com.callumwong.jishintray;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import java.net.InetSocketAddress;
 import java.net.URI;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class P2PQuakeClientTest {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         WebSocketServer server = new WebSocketServer(new InetSocketAddress(8080)) {
             @Override
             public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
@@ -26,11 +21,12 @@ class P2PQuakeClientTest {
             }
 
             @Override
-            public void onMessage(WebSocket webSocket, String s) {}
+            public void onMessage(WebSocket webSocket, String s) {
+            }
 
             @Override
             public void onError(WebSocket webSocket, Exception e) {
-                System.err.println("an error occurred on connection " + webSocket.getRemoteSocketAddress()  + ":" + e);
+                System.err.println("an error occurred on connection " + webSocket.getRemoteSocketAddress() + ":" + e);
             }
 
             @Override
