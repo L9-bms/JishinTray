f877ca90230b7300f13c7cd783ff2a62b22cb135
callum <exploit.exploit.02@gmail.com>
2025-03-17 10:23:30 +1100

    add button group for min intensity

--- src/main/java/com/callumwong/jishintray/frame/options/NotificationsPanel.java
+++ src/main/java/com/callumwong/jishintray/frame/options/NotificationsPanel.java
@@ -12,6 +12,18 @@ public class NotificationsPanel extends JPanel {
     NotificationsPanel() {
         Configuration config = AppConfig.getInstance().getConfig();
 
+        add(new JLabel(StringUtil.getLocalizedString("setting.appearance.theme")));
+        ButtonGroup minimumIntensity = new ButtonGroup();
+        int[] intensities = new int[]{10, 20, 30, 40, 50, 55, 60, 65, 70};
+        for (int intensity : intensities) {
+            JRadioButton intensityButton = new JRadioButton(
+                    StringUtil.scaleToString(intensity),
+                    config.getInt("minimum_intensity") == intensity
+            );
+            intensityButton.addActionListener(e -> config.setProperty("minimum_intensity", intensity));
 
+            minimumIntensity.add(intensityButton);
+            this.add(intensityButton);
+        }
     }
 }
